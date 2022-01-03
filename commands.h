06700136d9b8873219efc71f886aaadc3fc92f3b
hodamar <hod110@gmail.com>
2022-01-03 14:56:43 +0200

    imp of 3 subclasses of Command

--- commands.h
+++ commands.h
@@ -37,6 +37,8 @@ protected:
 	DefaultIO* dio;
     AnomalyDetectorData* data;
     string description;
+    FILE trainFile;
+    FILE testFile;
 public:
 	Command(DefaultIO* dio, AnomalyDetectorData* data):dio(dio), data(data){}
 	virtual void execute()=0;
@@ -47,9 +49,56 @@ public:
 // implement here your command classes
 class UploadCommand : public Command{
 public:
-    UploadCommand (DefaultIO* dio, AnomalyDetectorData* data) : Command(dio,data){};
-    virtual void execute(){};
-    virtual string getDes () {};
+    UploadCommand (DefaultIO* dio, AnomalyDetectorData* data) : Command(dio,data){
+        this->description = "1. upload a time series csv file";
+    };
+    virtual void execute(){
+        vector<string> train;
+        vector<string> test;
+        string line;
+        FILE f;
+        const char* trainName = "anomalyTrain.csv";
+        const char* testName = "anomalyTest.csv";
+        ///// TODO: do it with loop - it will be better
+
+        /// read train.
+        dio->write("Please upload your local train CSV file.\n");
+        line = dio->read();
+        while ("done" != line){
+            train.push_back(line);
+            line = dio->read();
+        }
+        makeFileFromVector(train, trainName);
+        TimeSeries* ts1 = new TimeSeries(trainName);
+        this->data->learnData = ts1;
+        dio->write("Upload complete.\n");
+
+        /// test train
+        dio->write("Please upload your local test CSV file.\n");
+        line = dio->read();
+        while ("done" != line){
+            test.push_back(line);
+            line = dio->read();
+        }
+        TimeSeries* ts2 = new TimeSeries(trainName);
+        this->data->TestDate = ts2;
+        makeFileFromVector(train, testName);
+        dio->write("Upload complete.\n");
+
+    };
+
+
+
+    virtual string getDes () {
+        return this->description;
+    };
+
+    FILE makeFileFromVector(vector<string> vec, const char* fileName){
+        ofstream file(fileName);
+        ofstream output_file(fileName);
+        ostream_iterator<std::string> output_iterator(output_file, "\n");
+        copy(vec.begin(), vec.end(), output_iterator);
+    }
 
 };
 class SettingsCommand : public Command {
@@ -59,9 +108,21 @@ class SettingsCommand : public Command {
 
 };
 class AnomalyDetectionCommand : public Command{
-    AnomalyDetectionCommand (DefaultIO* dio, AnomalyDetectorData* data) : Command(dio,data){};
-    virtual void execute(){};
-    virtual string getDes () {};
+
+
+AnomalyDetectionCommand (DefaultIO* dio, AnomalyDetectorData* data) : Command(dio,data){
+    this->description = "3.detect anomalies\n";
+};
+    virtual void execute(){
+
+        this->data->detector->learnNormal(*(this->data->learnData));
+        this->data->detector->learnNormal(*(this->data->TestDate));
+        this->dio->write("anomaly detection complete.\n");
+
+    };
+    virtual string getDes () {
+        return this->description;
+    };
 
 };
 class ResultCommand : public Command{
@@ -76,10 +137,15 @@ class AnalyzeCommand : public Command{
     virtual string getDes () {};
 
 };
+
 class ExitCommand : public Command{
-    ExitCommand (DefaultIO* dio, AnomalyDetectorData* data) : Command(dio,data){};
+    ExitCommand (DefaultIO* dio, AnomalyDetectorData* data) : Command(dio,data){
+        this->description = "6.exit\n";
+    };
     virtual void execute(){};
-    virtual string getDes () {};
+    virtual string getDes () {
+        return this->description;
+    };
 
 };
 
